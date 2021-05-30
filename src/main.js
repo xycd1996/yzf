a95e59161e7a1f3f548a66dd6a115e6e5a4484ad
kiter <515838758@qq.com>
2021-05-30 22:31:30 +0800

    修改环境配置

--- src/main.js
+++ src/main.js
@@ -6,12 +6,13 @@ import './configure'
 import router from './router'
 import store from './store'
 
-
 // import VConsole from 'vconsole'
 // new VConsole()
 
-localStorage.setItem('uid', '183701')
-localStorage.setItem('token', 'e668d884676fab5ce0f99dd7b8a02526')
+if (process.env.NODE_ENV !== 'production') {
+  localStorage.setItem('uid', '183701')
+  localStorage.setItem('token', '193cfa6cfd61791b296fb26abd459922')
+}
 
 new Vue({
   router,
